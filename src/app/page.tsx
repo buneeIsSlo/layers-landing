6dbfef22a8c769cd01d4cef9817aaff713a6deeb
abhi <buneeisslo@gmail.com>
2025-04-11 19:48:42 +0530

    init-0 lfg

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <h1 className="text-indigo-700 font-bold text-5xl">Layers</h1>
+      </main>
+    </div>
+  );
+}
