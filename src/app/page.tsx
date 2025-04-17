84dcb44484aa04347ff7505421db315809ccaa11
abhi <buneeisslo@gmail.com>
2025-04-17 21:06:52 +0530

    Add: navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,12 @@ import Intro from "@/sections/intro";
 import LogoTicker from "@/sections/logo-ticker";
 import CallToAction from "../sections/cta";
 import Footer from "@/sections/footer";
+import Navbar from "@/sections/navbar";
 
 export default function Home() {
   return (
     <>
+      <Navbar />
       <Hero />
       <LogoTicker />
       <Intro />
