a18c11cd661052764e8f4daeed544c3861a1cb9b
abhi <buneeisslo@gmail.com>
2025-04-18 22:52:58 +0530

    Add: hero animation to design imgs and cursors

--- src/sections/hero.tsx
+++ src/sections/hero.tsx
@@ -1,25 +1,100 @@
+"use client";
+
 import designExample1Image from "@/assets/images/design-example-1.png";
 import designExample2Image from "@/assets/images/design-example-2.png";
 import Button from "@/components/button";
 import Cursor from "@/components/cursor";
 import Image from "next/image";
+import { motion, useAnimate } from "motion/react";
+import { useRef, useEffect } from "react";
+import cursorYouImage from "@/assets/images/cursor-you.svg";
 
 export default function Hero() {
+  const [leftDesignScope, leftDesignAnimate] = useAnimate();
+  const [leftCursorScope, leftCursorAnimate] = useAnimate();
+  const [rightDesignScope, rightDesignAnimate] = useAnimate();
+  const [rightCursorScope, rightCursorAnimate] = useAnimate();
+  const constraintRef = useRef(null);
+
+  useEffect(() => {
+    leftDesignAnimate([
+      [leftDesignScope.current, { opacity: 1 }, { duration: 0.5 }],
+      [leftDesignScope.current, { y: 0, x: 0 }, { duration: 0.5 }],
+    ]);
+
+    leftCursorAnimate([
+      [leftCursorScope.current, { opacity: 1 }, { duration: 0.5 }],
+      [leftCursorScope.current, { y: 0, x: -100 }, { duration: 0.5 }],
+      [
+        leftCursorScope.current,
+        { x: 0, y: [0, 16, 0] },
+        { duration: 0.5, ease: "easeInOut" },
+      ],
+    ]);
+
+    rightDesignAnimate([
+      [rightDesignScope.current, { opacity: 1 }, { duration: 0.5, delay: 1.5 }],
+      [rightDesignScope.current, { x: 0, y: 0 }, { duration: 0.5 }],
+    ]);
+
+    rightCursorAnimate([
+      [rightCursorScope.current, { opacity: 1 }, { duration: 0.5, delay: 1.5 }],
+      [rightCursorScope.current, { x: 175, y: 0 }, { duration: 0.5 }],
+      [
+        rightCursorScope.current,
+        { x: 0, y: [0, 20, 0] },
+        { duration: 0.5, ease: "easeInOut" },
+      ],
+    ]);
+  }, []);
+
   return (
-    <section className="overflow-clip py-24">
+    <section
+      className="overflow-x-clip py-24"
+      style={{ cursor: `url(${cursorYouImage.src}), auto` }}
+      ref={constraintRef}
+    >
       <div className="relative container">
-        <div className="absolute top-16 -left-32 hidden lg:block">
-          <Image src={designExample1Image} alt="design example 1 image" />
-        </div>
-        <div className="absolute -top-16 -right-64 hidden lg:block">
-          <Image src={designExample2Image} alt="design example 2 image" />
-        </div>
-        <div className="absolute top-96 left-56 hidden lg:block">
+        <motion.div
+          ref={leftDesignScope}
+          initial={{ opacity: 0, y: 100, x: -100 }}
+          drag
+          dragConstraints={constraintRef}
+          className="absolute top-16 -left-32 hidden lg:block"
+        >
+          <Image
+            src={designExample1Image}
+            alt="design example 1 image"
+            draggable={false}
+          />
+        </motion.div>
+        <motion.div
+          ref={leftCursorScope}
+          initial={{ opacity: 0, y: 100, x: -200 }}
+          className="absolute top-96 left-56 hidden lg:block"
+        >
           <Cursor name="Andrea" />
-        </div>
-        <div className="absolute -top-4 right-80 hidden lg:block">
+        </motion.div>
+        <motion.div
+          ref={rightDesignScope}
+          initial={{ opacity: 0, x: 100, y: 100 }}
+          drag
+          dragConstraints={constraintRef}
+          className="absolute -top-16 -right-64 hidden lg:block"
+        >
+          <Image
+            src={designExample2Image}
+            alt="design example 2 image"
+            draggable={false}
+          />
+        </motion.div>
+        <motion.div
+          ref={rightCursorScope}
+          initial={{ opacity: 0, x: 275, y: 100 }}
+          className="absolute -top-4 right-80 hidden lg:block"
+        >
           <Cursor name="Bryan" color="red" />
-        </div>
+        </motion.div>
         <div className="flex items-center justify-center">
           <div className="inline-flex rounded-full bg-gradient-to-r from-purple-400 to-pink-400 px-3 py-1 font-semibold text-neutral-950">
             ✨ £7.5M seed round raised
@@ -42,7 +117,7 @@ export default function Hero() {
           <Button
             type="submit"
             variant="primary"
-            className="cursor-pointer whitespace-nowrap hover:opacity-90"
+            className="cursor-Cursor whitespace-nowrap hover:opacity-90"
             size="sm"
           >
             Sign Up
